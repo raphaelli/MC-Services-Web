cc826e62e06d417a3ba3e3b6438a20c582e672ea
raphaelli <raphael_li@live.com>
2017-12-27 22:50:17 +0800

    update

---
+++ MCServicesWeb/Models/UsersInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Web;
+
+namespace MCServicesWeb.Models
+{
+    public class UsersInfo
+    {
+        public int UsersId { get; set; }
+
+        public int UsersLevel { get; set; }
+
+        public string UsersName { get; set; }
+
+        public string UsersPwd { get; set; }
+
+        public int UsersSex { get; set; }
+
+        public DateTime UsersBirthday { get; set; }
+
+        public SqlMoney UsersCash { get; set; }
+
+        public string UsersAvator { get; set; }
+
+        public int UsersWhite { get; set; }
+
+        public int UsersDel { get; set; }
+
+    }
+
+    public class UsersDBContext : DbContext
+    {
+        public DbSet<UsersInfo> Users { get; set; }
+    }
+}
